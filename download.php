74d0ba2111f407ecb078838c009e7fe24a93e986
scrapgists <asishbm98@gmail.com>
2019-04-11 22:27:40 +0530

    File Upload success :)

--- download.php
+++ download.php
@@ -24,17 +24,15 @@ if($_SERVER["REQUEST_METHOD"] == "POST"){
 
     //To check filetype
     if($ext!=$allowed) {
-       die("Invalid Filetype!!!!");
+       // die("Invalid Filetype!!!!");
     }
 
     // Verify file size - 5MB maximum
     $maxsize = 1 * 1024 * 1024;
     if($filesize > $maxsize) die("Error: File size is larger than the allowed limit.");
 
-    if(in_array($filetype, $allowed))
-    {
-        move_uploaded_file($_FILES["file"]["tmp_name"], "/" . $filename);
+ 
+        move_uploaded_file($_FILES["file"]["tmp_name"], "upload/" . $filename);
         echo "Your file was uploaded successfully.";
-    }
-    else { die("FILETYPE ERROR!!!!"); }
+}
 ?>
